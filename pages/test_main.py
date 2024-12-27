a4bf011d005aa51842ac3ed6ab00d3c4161d69fd
ElizabethNa7 <elizabethna777@gmail.com>
2024-12-26 16:49:23 -0800

    added structure and started base of login pytest

--- pages/test_main.py
+++ pages/test_main.py
@@ -1,3 +1,4 @@
+import pytest 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -8,15 +9,10 @@ service = Service(executable_path="/Users/elizabeth-na/Downloads/chromedriver-ma
 driver = webdriver.Chrome(service=service)
 
 # Open a webpage
-driver.get("https://www.google.com/")
-
-# Find the search box, type a query, and hit Enter
-search_box = driver.find_element(By.NAME, "q")
-search_box.send_keys("Selenium Python")
-search_box.submit()
+driver.get("https://jira-selenium-test.atlassian.net/jira/your-work")
 
 # Wait for results to load
-time.sleep(5)
+time.sleep(10)
 
 # Close the browser
 driver.quit()
