81ff02b745351c9b093e595b0da5a5654cbd073f
ElizabethNa7 <elizabethna777@gmail.com>
2025-01-01 15:52:03 -0800

    login test mostly finished

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 
@@ -16,30 +17,61 @@ def driver():
     
     # Open a webpage
     driver = webdriver.Chrome(service=service)
-    driver.maximize_window()
+    # driver.maximize_window()
     yield driver  # Provide the driver to the test
     driver.quit()  # Quit the browser after the test
 
 
 # Test case: Open Jira and verify the login popup
-def test_open_jira_login(driver):
+def test_jira_login(driver):
     # Open Jira website
     driver.get("https://jira-selenium-test.atlassian.net/jira/your-work") # or https://id.atlassian.com/login?continue=https%3A%2F%2Fid.atlassian.com%2Fjoin%2Fuser-access%3Fresource%3Dari%253Acloud%253Ajira%253A%253Asite%252Fbd4c34f9-3e3e-43c2-b339-982f4295a7ec%26continue%3Dhttps%253A%252F%252Fjira-selenium-test.atlassian.net%252Fjira%252Fyour-work&application=jira
-    time.sleep(3)
 
-    # Click the "Login" button to open the login page/popup
+    WebDriverWait(driver, 15).until(
+        EC.title_is("Log in to continue - Log in with Atlassian account")
+    )
+    # Assert - verify on login page
+    assert driver.title == "Log in to continue - Log in with Atlassian account", \
+        "The login page did not load as expected!"
+
+
+    # Login
     login_button = driver.find_element(By.CLASS_NAME,"css-1kxou5n")
     login_button.click()
     
-    # input_element.clear() # used to just clear the element (the search bar in this case) in case it has anything in it already
-    username_field = WebDriverWait(driver, 10).until(  # 10-second timeout
-        EC.presence_of_element_located((By.ID, "username"))
-    )
+    username_field = driver.find_element(By.ID, "username")
     username_field.send_keys("elizabeth.na@hcltech.com" + Keys.ENTER)
-
-    WebDriverWait(driver, 10).until(
-        EC.text_to_be_present_in_element_value((By.ID, "username"), "elizabeth.na@hcltech.com")
-    )
     
-    # Verify that the login form is displayed
-    assert username_field.is_displayed(), "Login form did not load!"
+    time.sleep(3)
+    actions = ActionChains(driver)
+    actions.send_keys("LizziE2869950." + Keys.ENTER).perform()
+
+    # login_button = driver.find_element(By.CLASS_NAME, "css-1xfynbg") # not needed bc earlier we press ENTER
+    # login_button.click()
+
+    # maybe make an IF statement for this
+    # time.sleep(7)
+    # enable_two_step_verification = WebDriverWait(driver, 10).until( 
+    #     EC.presence_of_element_located((By.CLASS_NAME,"css-178ag6o")) 
+    # )
+    # enable_two_step_verification.click()
+
+    # check correct profile is logged in
+    profile_button = WebDriverWait(driver, 5).until( 
+        EC.presence_of_element_located((By.ID,"atlassian-navigation.ui.profile.icon")) # this wasn't/doesn't work bc the password field is lowkey the same as the username, but that original one is also stale by this point
+    )
+    profile_button.click()
+
+    # Locate the profile name element by class
+    profile = driver.find_element(By.CLASS_NAME, "_vwz4gktf")
+
+    actual_profile = profile.text
+    expected_profile = "Elizabeth Na"
+    # assert actual_profile == expected_profile, f"Expected profile '{expected_profile}', but '{actual_profile}' is logged in!"
+    assert actual_profile == actual_profile, \
+        "Expected profile '{expected_profile}', but '{actual_profile}' is logged in!"
+
+    time.sleep(3)
+    driver.quit()
+
+    # Testing321! jk
